37f85b0a6b7664b3b8458812b781f6e6520450ff
Craig Burke <cragardev@gmail.com>
2024-10-20 15:09:25 -0500

    computer game logic worked out

--- computer_player.py
+++ computer_player.py
@@ -2,18 +2,36 @@ from random import randint, choice, shuffle, random
 
 
 class ComputerPlayer:
-    def __init__(self, difficulty="easy"):
+    def __init__(self, difficulty="easy", player="", gameboard=None):
         self.difficulty = difficulty
+        self.player = player
+        self.gameboard = gameboard
 
     def select_square(self, game_board):
         if self.difficulty == "easy":
-            return self.random_move(game_board)
+            return self.easy_move(game_board)
         elif self.difficulty == "medium":
             return self.medium_move(game_board)
         elif self.difficulty == "hard":
             return self.hard_move(game_board)
 
     def easy_move(self, game_board):
+        print("--- Making an easy move ---")
+        print(f"Player: {self.player}")
+        print()
+        # make a random selection from any of the empty squares
+        empty_squares = get_empty_squares()
+        computer_choice = choice(empty_squares)
+        # return the computer choice of the game_matrix_dict
+        for key, value in game_matrix_dict.items():
+            if value[0] == computer_choice:
+                # set the computer choice to "O"
+                set_selection(computer_choice[0], computer_choice[1], player)
+                # game_matrix_dict[key][1] = True
+                # game_matrix_dict[key][2] = player
+                # print_matrix()
+        print(f"Game Matrix: {game_matrix_dict}")
+
         # make a random selection from any of the empty squares
         empty_squares = game_board.get_empty_squares()
 
--- gameboard_TESTING01.py
+++ gameboard_TESTING01.py
@@ -1,4 +1,5 @@
 from random import randint, choice, shuffle, random
+import time
 
 
 def reset_matrix():
@@ -122,7 +123,7 @@ def medium_move(player: str):
     # print(f"Available Squares: {available_squares}")
     print()
 
-    adjacent_squares = []
+    opponent_adjacent_squares = []
     # get a list of squares that are adjacent to the opponent_squares
     for square in opponent_squares:
         # print(f"SQUARE: {square}")
@@ -158,13 +159,13 @@ def medium_move(player: str):
                 or game_matrix_dict[key][0]
                 == (square[0] + 1, square[1] - 1)  # bottom left
             ):
-                # check that the square is not a computer square or an opponent square and not already in the adjacent_squares list
+                # check that the square is not a computer square or an opponent square and not already in the opponent_adjacent_squares list
                 if (
                     game_matrix_dict[key][0] not in computer_squares
                     and game_matrix_dict[key][0] not in opponent_squares
-                    and game_matrix_dict[key][0] not in adjacent_squares
+                    and game_matrix_dict[key][0] not in opponent_adjacent_squares
                 ):
-                    adjacent_squares.append(game_matrix_dict[key][0])
+                    opponent_adjacent_squares.append(game_matrix_dict[key][0])
 
 
 def hard_move(player: str):
@@ -183,31 +184,22 @@ def hard_move(player: str):
     # then it returns that empty square to win the game or block the other player from winning
     # 2. if no winning move is found, then it gets the empty squares that are adjacent to the player's squares, if an empty square is next to a players square, then it puts that square in the list of possible moves (this is medium difficulty, that's why it doesn't check for the player's winning moves)
 
+    # -- get a list of the empty squares
     empty_squares = get_empty_squares()
-    # check for a winning move for the computer player
-    # check if player is on the board at all first
+
+    # -- check if this is the first move of the game
     print(f"Length of empty_squares: {len(empty_squares)}")
     if len(empty_squares) == 9:
         computer_choice = choice(empty_squares)
         set_selection(computer_choice[0], computer_choice[1], player)
-        # return the computer choice of the game_matrix_dict
-        # for key, value in game_matrix_dict.items():
-        #     if value[0] == computer_choice:
-        #         # set the computer choice to "O"
-        #         game_matrix_dict[key][1] = True
-        #         game_matrix_dict[key][2] = player
-        #         print_matrix()
         print(f"Game Matrix: {game_matrix_dict}")
         return
 
-    # get a list of squares that are adjacent to the human player (not the computer player)
-    # print()
-    # print(f"__ before: Game Matrix: {game_matrix_dict}")
-    # print()
+    # -- list to store the opponent squares and the player squares
     opponent_squares = []
     player_squares = []
-    block_or_win_squares = []
 
+    # -- fill the opponent_squares and player_squares lists
     for key, value in game_matrix_dict.items():
         if game_matrix_dict[key][2] != player and game_matrix_dict[key][1] == True:
             opponent_squares.append(game_matrix_dict[key][0])
@@ -216,31 +208,81 @@ def hard_move(player: str):
 
     print(f"Opponent Squares: {opponent_squares}")
     print(f"Player Squares: {player_squares}")
-    # print(f"Available Squares: {available_squares}")
     print()
 
-    # check if the player has two squares adjacent to each other either horizontally, vertically or diagonally
+    # -- check if the player has two squares adjacent to each other either horizontally, vertically or diagonally
     print(f"Length of player_squares: {len(player_squares)}")
     if len(player_squares) >= 2:
         has_two = check_for_two(player)
-        print(f"Has Two - Open Square: {has_two}")
-        if has_two:
-            # set the square selection to the has_two
-            set_selection(has_two[0][0], has_two[0][1], player)
-            print(f"Game Matrix: {game_matrix_dict}")
-            return
-    # check if the opponent has two squares adjacent to each other either horizontally, vertically or diagonally
-    print(f"Length of opponent_squares: {len(opponent_squares)}")
-    if len(opponent_squares) >= 2:
-        has_two = check_for_two(opponent)
-        print(f"Has Two - Open Square: {has_two}")
+        print(f"Player Has Two Adjacent Squares - The Open Square is: {has_two}")
         if has_two:
             # set the square selection to the has_two
             set_selection(has_two[0][0], has_two[0][1], player)
             print(f"Game Matrix: {game_matrix_dict}")
             return
 
-    adjacent_squares = []
+    # -- check if the opponent has two squares adjacent to each other either horizontally, vertically or diagonally
+    print(f"Length of opponent_squares: {len(opponent_squares)}")
+    if len(opponent_squares) >= 2:
+        has_two = check_for_two(opponent)
+        print(f"Opponent Has Two Squares Adjacent - The Open Square is: {has_two}")
+        if has_two:
+            # set the square selection for the blocking move
+            set_selection(has_two[0][0], has_two[0][1], player)
+            print(f"Game Matrix: {game_matrix_dict}")
+            return
+
+    # --check that there are empty adjacent squares to the player squares
+    if len(player_squares) >= 1:
+        # get the empty squares that are adjacent to the player squares
+        player_adjacent_squares = []
+        # get a list of squares that are adjacent to the player_squares
+        for square in player_squares:
+            for key, value in game_matrix_dict.items():
+                # Check each square around the opponent square (row, col) - (0,0)
+                #    -1,-1    -1,0    -1,+1
+                #     0,-1     0,0     0,+1
+                #    +1,-1    +1,0    +1,+1
+                if (
+                    game_matrix_dict[key][0] == (square[0] - 1, square[1])  # top
+                    or game_matrix_dict[key][0] == (square[0] + 1, square[1])  # bottom
+                    or game_matrix_dict[key][0] == (square[0], square[1] - 1)  # left
+                    or game_matrix_dict[key][0] == (square[0], square[1] + 1)  # right
+                    or game_matrix_dict[key][0]
+                    == (square[0] - 1, square[1] - 1)  # top left
+                    or game_matrix_dict[key][0]
+                    == (square[0] + 1, square[1] + 1)  # bottom right
+                    or game_matrix_dict[key][0]
+                    == (square[0] - 1, square[1] + 1)  # top right
+                    or game_matrix_dict[key][0]
+                    == (square[0] + 1, square[1] - 1)  # bottom left
+                ):
+
+                    # check that the square is not a computer square or an opponent square and not already in the opponent_adjacent_squares list
+                    if (
+                        game_matrix_dict[key][0] not in player_squares
+                        and game_matrix_dict[key][0] not in opponent_squares
+                        and game_matrix_dict[key][0] not in player_adjacent_squares
+                    ):
+                        player_adjacent_squares.append(game_matrix_dict[key][0])
+
+        print()
+        print(f"Player Adjacent Squares: {player_adjacent_squares}")
+        print()
+
+        # Now make a choice of the adjacent squares
+        player_adj_choice = choice(player_adjacent_squares)
+        print(f"Oppenent Adjacent Squares Choice: {player_adj_choice}")
+        print()
+
+        # set the square selection to the adj_choice
+        set_selection(player_adj_choice[0], player_adj_choice[1], player)
+
+        print(f"Game Matrix: {game_matrix_dict}")
+        return
+
+    # --if there are no empty squares adjacent to the player squares, then check for empty squares adjacent to the opponent squares
+    opponent_adjacent_squares = []
     # get a list of squares that are adjacent to the opponent_squares
     for square in opponent_squares:
         # print(f"SQUARE: {square}")
@@ -258,9 +300,9 @@ def hard_move(player: str):
         for key, value in game_matrix_dict.items():
             # Check each square around the opponent square (row, col) - (0,0)
 
-            #             -1,-1    -1,0    -1,+1
-            #              0,-1    0,0    0,+1
-            #             +1,-1    +1,0    +1,+1
+            #    -1,-1    -1,0    -1,+1
+            #     0,-1     0,0     0,+1
+            #    +1,-1    +1,0    +1,+1
 
             if (
                 game_matrix_dict[key][0] == (square[0] - 1, square[1])  # top
@@ -273,28 +315,28 @@ def hard_move(player: str):
                 == (square[0] + 1, square[1] + 1)  # bottom right
                 or game_matrix_dict[key][0]
                 == (square[0] - 1, square[1] + 1)  # top right
-                or game_matrix_dict[key][0]
-                == (square[0] + 1, square[1] - 1)  # bottom left
-            ):
-                # check that the square is not a computer square or an opponent square and not already in the adjacent_squares list
+                or game_matrix_dict[key][0] == (square[0] + 1, square[1] - 1)
+            ):  # bottom left
+
+                # check that the square is not a computer square or an opponent square and not already in the opponent_adjacent_squares list
                 if (
                     game_matrix_dict[key][0] not in player_squares
                     and game_matrix_dict[key][0] not in opponent_squares
-                    and game_matrix_dict[key][0] not in adjacent_squares
+                    and game_matrix_dict[key][0] not in opponent_adjacent_squares
                 ):
-                    adjacent_squares.append(game_matrix_dict[key][0])
+                    opponent_adjacent_squares.append(game_matrix_dict[key][0])
 
     print()
-    print(f"Adjacent Squares: {adjacent_squares}")
+    print(f"Opponent Adjacent Squares: {opponent_adjacent_squares}")
     print()
 
     # Now make a choice of the adjacent squares
-    adj_choice = choice(adjacent_squares)
-    print(f"Adjacent Squares Choice: {adj_choice}")
+    opponent_adj_choice = choice(opponent_adjacent_squares)
+    print(f"Oppenent Adjacent Squares Choice: {opponent_adj_choice}")
     print()
 
     # set the square selection to the adj_choice
-    set_selection(adj_choice[0], adj_choice[1], player)
+    set_selection(opponent_adj_choice[0], opponent_adj_choice[1], player)
 
     print(f"Game Matrix: {game_matrix_dict}")
     return
@@ -302,6 +344,7 @@ def hard_move(player: str):
 
 def check_for_two(player: str):
     # check for 2 squares in a row, column or diagonal for the player
+    # returns the first one found
     # returns the third square if found and None if not found
     for key, value in game_matrix_dict.items():
         # check the rows
@@ -597,6 +640,54 @@ def game_over_winner(player):
     exit()
 
 
+def run_game():
+    print()
+    print("o" * 50)
+    print("Tic-Tac-Toe Game")
+    print("o" * 50)
+    print()
+
+    print()
+    print("=== Setting up the game matrix ===")
+    for key, item in game_matrix_dict.items():
+        print(key, item)
+
+    print("=================================")
+    print()
+
+    click_count = 0
+    print_matrix()
+    print()
+
+    while click_count < 9:
+        hard_move("X")
+        click_count += 1
+        print()
+        if check_for_winner("X"):
+            game_over_winner("X")
+        if click_count == 9:
+            game_over_draw()
+        # input("Press any key to continue...")
+        wait_seconds(2)
+
+        print()
+        hard_move("O")
+        click_count += 1
+        print()
+        if check_for_winner("O"):
+            game_over_winner("O")
+        if click_count == 9:
+            game_over_draw()
+        # input("Press any key to continue...")
+        wait_seconds(2)
+        print()
+    game_over_draw()
+
+
+def wait_seconds(seconds):
+    time.sleep(seconds)
+
+
 game_matrix_dict = {
     "sq1": [(0, 0), False, ""],  # [position, is_clicked, player]
     "sq2": [(0, 1), False, ""],
@@ -609,52 +700,4 @@ game_matrix_dict = {
     "sq9": [(2, 2), False, ""],
 }
 
-
-print()
-print("o" * 50)
-print("Tic-Tac-Toe Game")
-print("o" * 50)
-print()
-
-print()
-print("=== Setting up the game matrix ===")
-for key, item in game_matrix_dict.items():
-    print(key, item)
-
-print("=================================")
-print()
-
-click_count = 0
-print_matrix()
-print()
-# medium_move("X")
-# print()
-# medium_move("O")
-# print()
-# easy_move("X")
-# print()
-# medium_move("O")
-# print()
-# input("Press any key to continue...")
-# print()
-
-while click_count < 9:
-    hard_move("X")
-    click_count += 1
-    print()
-    if check_for_winner("X"):
-        game_over_winner("X")
-    if click_count == 9:
-        game_over_draw()
-    input("Press any key to continue...")
-    print()
-    hard_move("O")
-    click_count += 1
-    print()
-    if check_for_winner("O"):
-        game_over_winner("O")
-    if click_count == 9:
-        game_over_draw()
-    input("Press any key to continue...")
-    print()
-game_over_draw()
+run_game()
